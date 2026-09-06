5e6d3c30b453f553b0f7ec424c2052b4c77feec8
Alejandro González Zapico <alejandrogonzalezzapico@gmail.com>
2026-09-06 12:51:21 +0200

    style: load mobile polish overrides last

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './room-mark-overrides.css';
 import './header-icon-polish.css';
 import './map-tools.css';
 import './mobile.css';
+import './mobile-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
